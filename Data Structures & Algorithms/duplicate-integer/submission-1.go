687d619d308b09b6272aa388ee32f0e09c71bd34
Fattesingh Rane <41385129+ranefattesingh@users.noreply.github.com>
2026-06-06 21:01:12 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.go
@@ -0,0 +1,13 @@
+func hasDuplicate(nums []int) bool {
+    hashMap := make(map[int]struct{})
+    for _, v := range nums {
+        _, ok := hashMap[v]
+        if ok {
+            return true
+        }
+
+        hashMap[v] = struct{}{}
+    }
+
+    return false
+}
