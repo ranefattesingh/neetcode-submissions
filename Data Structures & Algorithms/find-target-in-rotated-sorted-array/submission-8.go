c25506fb19a9db77296fe6dcae84a293f6d4e77e
Fattesingh Rane <41385129+ranefattesingh@users.noreply.github.com>
2026-06-18 16:51:26 +0530

    Add: find-target-in-rotated-sorted-array - submission-8

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-8.go
@@ -0,0 +1,27 @@
+func search(nums []int, target int) int {
+    l, r := 0, len(nums) - 1
+    
+    for l <= r {
+        m := l + (r-l)/2
+
+        if target == nums[m] {
+            return m
+        }
+
+        if nums[l] <= nums[m] {
+            if target > nums[m] || target < nums[l] {
+                l = m + 1
+            } else {
+                r = m - 1
+            }
+        } else {
+            if target < nums[m] || target > nums[r] {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+    }
+
+    return -1
+}
