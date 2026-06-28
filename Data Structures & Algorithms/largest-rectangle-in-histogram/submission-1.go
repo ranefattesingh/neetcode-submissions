f5c26952fe75600cd0189a424b675fae8ef10bac
Fattesingh Rane <41385129+ranefattesingh@users.noreply.github.com>
2026-06-28 22:44:20 +0530

    Add: largest-rectangle-in-histogram - submission-1

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-1.go
@@ -0,0 +1,25 @@
+func largestRectangleArea(heights []int) int {
+	stack := []int{}
+
+	res := 0
+
+	for i := 0; i <= len(heights); i++ {
+		for len(stack) > 0 && (i == len(heights) || heights[stack[len(stack)-1]] >= heights[i]){
+			smallest := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			width := i
+			if len(stack) > 0 {
+				width = i - stack[len(stack)-1] - 1
+			}
+
+			res = max(res, heights[smallest]*width)
+		}
+
+		if i < len(heights) {
+			stack = append(stack, i)
+		}
+	}
+
+	return res
+}
