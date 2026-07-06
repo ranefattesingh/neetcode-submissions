5772441af6f88ccd653eda1bf33524c67f7a8e33
Fattesingh Rane <41385129+ranefattesingh@users.noreply.github.com>
2026-07-06 14:29:21 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.go
@@ -0,0 +1,19 @@
+func searchMatrix(matrix [][]int, target int) bool {
+    rows, cols := len(matrix), len(matrix[0])
+    l, r := 0, rows*cols-1
+
+    for l <= r {
+        m := l+(r-l)/2
+        row, col := m/cols, m%cols
+        val := matrix[row][col]
+        if val == target {
+            return true
+        } else if val < target {
+            l = m + 1
+        } else {
+            r = m - 1
+        }
+    }
+
+    return false
+}
